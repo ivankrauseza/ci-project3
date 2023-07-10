b57b5cbf6848d6802c35ef208a31505b584ac33b
Ivan Krause <ivan.krause@gmail.com>
2023-07-11 00:26:34 +0100

    add simple options selector

--- run.py
+++ run.py
@@ -17,3 +17,24 @@ else:
 name = input("Please enter your name: ")
 print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
 
+user_input = ''
+
+while True:
+    user_input = input(
+        'Pick one: 1) Python | 2) JavaScript | 3) TypeScript [1/2/3]? ')
+
+    if user_input == '1':
+        print('You picked Python')
+        break
+    elif user_input == '2':
+        print('You picked JavaScript')
+        break
+    elif user_input == '3':
+        print('You picked TypeScript')
+        break
+
+    else:
+        print('Type a number 1-3')
+        continue
+
+
