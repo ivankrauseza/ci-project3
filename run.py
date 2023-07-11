f41dc90ca507a51f9462265363dbc672a4a5c3e5
Ivan Krause <ivan.krause@gmail.com>
2023-07-11 23:13:01 +0100

    trying to get the sequence correct due to first bug

--- run.py
+++ run.py
@@ -30,59 +30,67 @@ elif afternoon_start <= current_time < evening_start:
 else:
     greeting = "\nGood evening"
 
-# Step 1: Ask for the user name:
-def askName():
-    name = input("Please enter your Agent designation: ")
-    if name == "007":
-        print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
-    elif name == "Agent 47":
-        print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
-    else:
-        print("Your session cannot be started")
 
-askName()
 
 # Answer 1
 def getTarget():
-    print('Your next target is PROFILE')
+    print('\nYour next target is PROFILE\n')
+    continueSession()
 
 # Answer 2
 def getCar():
-    print('You parked your car at your local Supermarket')
+    print('\nYou parked your car at your local Supermarket\n')
+    continueSession()
 
 # Answer 3
 def getTreat():
-    print('Something from Q-Branch')
+    print('\nSomething from Q-Branch\n')
+    continueSession()
+
 
 # All Options
 def showQuestions():
     options = ["1. Who is my next Target", "2. Where is my closest car", "3. I need a treat from Q-Branch", "4. End session"]
     for x in options:
-        print(x+"\n")
+        print(x+"")
 
-showQuestions()
+# Step 1: Ask for the user name:
+def askName():
+    name = input("Please enter your Agent Code: ")
+    if name == "007":
+        print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
+        showQuestions()
+    elif name == "47":
+        print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
+        showQuestions()
+    elif name == "":
+        print("Your session cannot be started, please enter a valid Agent Code!")
+        askName()
+    else:
+        print("Your session cannot be started")
+        askName()
+
+askName()
 
 
 # Question response:
 user_input = ''
 
 while True:
-    user_input = input('Please enter option: ')
-    if user_input == '1':
-        print('You picked JavaScript')
-        showQuestions()
-        break
-    elif user_input == '2':
+    user_input = int(input('\nPlease enter option: '))
+    if user_input == 1:
+        getTarget()
+        continue
+    elif user_input == 2:
         getCar()
-        showQuestions()
-        break
-    elif user_input == '3':
-        print('You picked TypeScript')
-        showQuestions()
-        break
-    elif user_input == '4':
+        continue
+    elif user_input == 3:
+        getTreat()
+        continue
+    elif user_input == 4:
         print('\nSession ended\n')
         break
     else:
         print('Type a number 1-4 please')
         continue
+
