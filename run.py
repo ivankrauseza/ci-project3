6d2b7b080b425324f46f5b35b25416fdd2637302
Ivan Krause <ivan.krause@gmail.com>
2023-07-11 22:48:43 +0100

    reorder sequence of functions

--- run.py
+++ run.py
@@ -1,44 +1,5 @@
 import datetime
 
-# Determine time based greeting message:
-current_time = datetime.datetime.now().time()
-morning_start = datetime.time(6, 0)
-afternoon_start = datetime.time(12, 0)
-evening_start = datetime.time(18, 0)
-start_time = current_time.strftime("%H:%M:%S")
-
-if morning_start <= current_time < afternoon_start:
-    greeting = "\nGood morning"
-elif afternoon_start <= current_time < evening_start:
-    greeting = "\nGood afternoon"
-else:
-    greeting = "\nGood evening"
-
-# Answer 1
-def getTarget():
-    print('Your next target is PROFILE')
-
-# Answer 2
-def getCar():
-    print('You parked your car at your local Supermarket')
-
-# Answer 3
-def getTreat():
-    print('Something from Q-Branch')
-
-# Step 1: Ask for the user name:
-def askName():
-    name = input("Please enter your Agent designation: ")
-    if name == "007":
-        print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
-    elif name == "Agent 47":
-        print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
-    else:
-        print("Your session cannot be started")
-
-askName()
-
-
 # Session Management
 def continueSession():
     qbranch = ''
@@ -55,7 +16,43 @@ def continueSession():
             showQuestions()
             continue
 
+# Determine time based greeting message:
+current_time = datetime.datetime.now().time()
+morning_start = datetime.time(6, 0)
+afternoon_start = datetime.time(12, 0)
+evening_start = datetime.time(18, 0)
+start_time = current_time.strftime("%H:%M:%S")
 
+if morning_start <= current_time < afternoon_start:
+    greeting = "\nGood morning"
+elif afternoon_start <= current_time < evening_start:
+    greeting = "\nGood afternoon"
+else:
+    greeting = "\nGood evening"
+
+# Step 1: Ask for the user name:
+def askName():
+    name = input("Please enter your Agent designation: ")
+    if name == "007":
+        print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
+    elif name == "Agent 47":
+        print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
+    else:
+        print("Your session cannot be started")
+
+askName()
+
+# Answer 1
+def getTarget():
+    print('Your next target is PROFILE')
+
+# Answer 2
+def getCar():
+    print('You parked your car at your local Supermarket')
+
+# Answer 3
+def getTreat():
+    print('Something from Q-Branch')
 
 # All Options
 def showQuestions():
@@ -89,5 +86,4 @@ while True:
     else:
         print('Type a number 1-4 please')
         continue
-
-
+    
