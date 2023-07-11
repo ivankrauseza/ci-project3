23c1b45e1b12110a3e11919df1061c6451bf85fe
Ivan Krause <ivan.krause@gmail.com>
2023-07-11 22:44:46 +0100

    add basic questions and answers

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import datetime
 
+# Determine time based greeting message:
 current_time = datetime.datetime.now().time()
 morning_start = datetime.time(6, 0)
 afternoon_start = datetime.time(12, 0)
@@ -7,13 +8,25 @@ evening_start = datetime.time(18, 0)
 start_time = current_time.strftime("%H:%M:%S")
 
 if morning_start <= current_time < afternoon_start:
-    greeting = "Good morning"
+    greeting = "\nGood morning"
 elif afternoon_start <= current_time < evening_start:
-    greeting = "Good afternoon"
+    greeting = "\nGood afternoon"
 else:
-    greeting = "Good evening"
+    greeting = "\nGood evening"
 
-# ENTER YOUR NAME
+# Answer 1
+def getTarget():
+    print('Your next target is PROFILE')
+
+# Answer 2
+def getCar():
+    print('You parked your car at your local Supermarket')
+
+# Answer 3
+def getTreat():
+    print('Something from Q-Branch')
+
+# Step 1: Ask for the user name:
 def askName():
     name = input("Please enter your Agent designation: ")
     if name == "007":
@@ -22,27 +35,59 @@ def askName():
         print(greeting+" "+name+", Your session is logged at: " +start_time+".\n")
     else:
         print("Your session cannot be started")
-        
+
 askName()
 
+
+# Session Management
+def continueSession():
+    qbranch = ''
+    while True:
+        qbranch = input("Would you like to continue? [Y/N]: ")
+        if qbranch == 'Y':
+            showQuestions()
+            continue
+        elif qbranch == 'N':
+            print("\nYour session has be terminated!\n")
+            break
+        else:
+            print("Please only enter Y for 'yes' or N for 'no'")
+            showQuestions()
+            continue
+
+
+
+# All Options
+def showQuestions():
+    options = ["1. Who is my next Target", "2. Where is my closest car", "3. I need a treat from Q-Branch", "4. End session"]
+    for x in options:
+        print(x+"\n")
+
+showQuestions()
+
+
+# Question response:
 user_input = ''
 
 while True:
-    user_input = input(
-        'Pick one: 1) Python | 2) JavaScript | 3) TypeScript [1/2/3]? ')
-
+    user_input = input('Please enter option: ')
     if user_input == '1':
-        print('You picked Python')
+        print('You picked JavaScript')
+        continueSession()
         break
     elif user_input == '2':
-        print('You picked JavaScript')
+        getCar()
+        continueSession()
         break
     elif user_input == '3':
         print('You picked TypeScript')
+        continueSession()
         break
-
+    elif user_input == '4':
+        print('Session ended')
+        break
     else:
-        print('Type a number 1-3')
+        print('Type a number 1-4 please')
         continue
 
 
